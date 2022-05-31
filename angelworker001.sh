49045638cdb03220ec269bb0533239823a5e1cb3
angelsinpc <106614203+angelsinpc@users.noreply.github.com>
2022-05-31 10:20:15 -0700

    tar hyphn added

--- angelworker001.sh
+++ angelworker001.sh
@@ -5,6 +5,6 @@ sudo apt install screen
 screen -R xmr
  
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
-tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
+tar -xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
 ./xmrig --cuda -o xmr.2miners.com:2222 -u 83bGWn25vGMB8PMKgPEpTzHvktfx39rG8KfhJjuKBmEH1iucJfXHTjUdsnqq5mmBuCNxTpjULARYa6GUHV1g29oYJ386xa8 -k --tls --rig-id happyming001
